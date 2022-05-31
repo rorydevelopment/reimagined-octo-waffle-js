0227d4ded549ece592c9b0252db8fc8165c9b841
rorydevelopment <44748089+rorydevelopment@users.noreply.github.com>
2022-05-31 14:45:21 +0100

    html page added with button element and unordered list. main js file updated to include onclick function for html button and code which will run when the window is loaded.

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+window.onload = callFunctions();
+
 console.log("Hello World!")
+
+for(let i = 0; i < 10; i++) {
+    console.log("This is number: " + i);
+}
+
+// this function will call specified
+// funcitons then the browser loads up
+function callFunctions() {
+    listCars();
+}
+
+// this function ois trigger on the click
+// event of the specified button
+function clickTestFunction() {
+    let myElement = document.getElementById("testId");
+    myElement.innerHTML = "I told you it would change!"
+}
+
+function listCars() {
+    let list = getElementById("listCars").children;
+    let count = list.length;
+    for(let i = 0; i < count; i++) {
+        console.log(list[i]);
+    }
+}
